d091e6b25e24abd0adce3d58dedfacb754a41195
Bohdan <blazarchuk@gmail.com>
2025-11-21 20:50:01 +0100

    Add meÑohds ito UserRepository

--- src/app/UserRepository.java
+++ src/app/UserRepository.java
@@ -1,4 +1,33 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class UserRepository {
+    User user;
+
+    List<User> userList = Arrays.asList(
+            new User(1, "Vasya", "vasya@com"),
+            new User(2, "Kolya", "kolya@com"),
+            new User(3, "Roman", "roman@com"),
+            new User(4, "Anton", "anton@com")
+    );
+
+    public User findUserById(int id) {
+        for (User user: userList){
+            if(user.getId() == id)
+                return user;
+        }
+        return null;
+    }
+
+    public User findUserByEmail(String email){
+        for (User user: userList){
+            if(user.getEmail() == email)
+                return user;
+        }
+        return null;
+    }
+
 }
